f77f5281d72c3d5241b95a3eba54806dddd02a91
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2019-11-11 07:34:01 +0200

    Add a `name` property for resolvers

--- resolvers.js
+++ resolvers.js
@@ -12,68 +12,71 @@ const assignParent = node =>
   node.parent
 
 const testParentType = type => node =>
-  node.parent && (node.parent.type === type)
+  node.parent && node.parent.type === type
 
 const testParentTypeBegins = type => node =>
   node.parent && node.parent.type.startsWith(type)
 
 
-const parentTypeResolver = (type) => ({
-  test:   testParentType(type),
+const returnResolver = {
+  name:   'Return resolver',
+  test:   testParentType('ReturnStatement'),
   assign: assignParent
-})
-
-const returnResolver = parentTypeResolver('ReturnStatement')
+}
 
 const variableResolver = {
+  name:   'variableResolver',
   test:   testParentType('VariableDeclaration'),
   assign: assignParent,
 }
 
 const variableDeclaratorResolver = {
+  name:   'variableDeclaratorResolver',
   test:   testParentType('VariableDeclarator'),
   assign: assignParent,
 }
 
 const exportResolver = {
+  name:   'exportResolver',
   test:   testParentTypeBegins('Export'),
   assign: assignParent,
 }
 
 const assignmentResolver = {
-
-  assign: node => node.parent.left,
+  name:   'assignmentResolver',
   test:   testParentType('AssignmentExpression'),
+  assign: node => node.parent.left,
 }
 
 const callExpressionResolver = {
-
-  assign: node => node.parent.parent,
+  name:   'callExpressionResolver',
   test:   testParentType('CallExpression'),
+  assign: node => node.parent.parent,
 }
 
 const arrowFunctionExpressionResolver = {
+  name:   'arrowFunctionExpressionResolver',
   test:   testParentType('ArrowFunctionExpression'),
   assign: assignParent,
 }
 
 const objectPropertyResolver = {
+  name:   'objectPropertyResolver',
   test:   testParentType('Property'),
   assign: assignParent,
 }
 
 const switchCaseResolver = {
+  name:   'switchCaseResolver',
   test:   testParentType('SwitchCase'),
   assign: assignParent,
 }
 
 const decoratorResolver = {
-
-  assign:   node => getFirstDecorator(node),
-
-  test:     node => node.decorators && node.decorators.length,
-
+  name:     'decoratorResolver',
   terminal: true,
+  assign:   node => getFirstDecorator(node),
+  test:     node => node.decorators && node.decorators.length,
 }
 
 module.exports = [
